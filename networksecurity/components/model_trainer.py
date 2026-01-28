495d39e347da89a791c05e143902cd67941c4453
Priyanshuc26 <priyanshuc2606@gmail.com>
2026-01-29 02:51:29 +0530

    mlflow added

---
+++ networksecurity/components/model_trainer.py
@@ -0,0 +1,145 @@
+import os
+import sys
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+from networksecurity.entity.artifact_entity import DataTransformationArtifact, ModelTrainerArtifact
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.main_utils.utils import save_object, load_object, evaluate_models
+from networksecurity.utils.main_utils.utils import load_numpy_array_data
+from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import (
+RandomForestClassifier,
+AdaBoostClassifier,
+GradientBoostingClassifier,
+)
+from sklearn.metrics import r2_score
+import mlflow
+
+
+class ModelTrainer:
+    def __init__(self, model_trainer_config: ModelTrainerConfig, data_transformation_artifact: DataTransformationArtifact,):
+        try:
+            self.model_trainer_config = model_trainer_config
+            self.data_transformation_artifact = data_transformation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+
+    ## Track the mlflow
+    def track_mlflow(self, best_model, classification_metric):
+        with mlflow.start_run():
+            f1_score = classification_metric.f1_score
+            precision = classification_metric.precision_score
+            recall = classification_metric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision)
+            mlflow.log_metric("recall", recall)
+            mlflow.sklearn.log_model(best_model, "model")
+
+
+    def train_model(self, x_train, y_train, x_test, y_test):
+        models = {
+            "LogisticRegression": LogisticRegression(verbose=1),
+            "RandomForestClassifier": RandomForestClassifier(verbose=1),
+            "AdaBoostClassifier": AdaBoostClassifier(),
+            "GradientBoostingClassifier": GradientBoostingClassifier(verbose=1),
+            "DecisionTreeClassifier": DecisionTreeClassifier(),
+            # "KNeighborsClassifier": KNeighborsClassifier(),
+            # "GaussianNB": GaussianNB(),
+            # "SVC": SVC(),
+        }
+
+        params = {
+            "DecisionTreeClassifier": {
+                'criterion': ['gini', 'entropy', 'log_loss'],
+                # 'splitter':['best','random'],
+                # 'max_features':['sqrt','log2'],
+            },
+            "RandomForestClassifier": {
+                # 'criterion':['gini', 'entropy', 'log_loss'],
+
+                # 'max_features':['sqrt','log2',None],
+                'n_estimators': [8, 16, 32, 128, 256]
+            },
+            "GradientBoostingClassifier": {
+                # 'loss':['log_loss', 'exponential'],
+                'learning_rate': [.1, .01, .05, .001],
+                'subsample': [0.6, 0.7, 0.75, 0.85, 0.9],
+                # 'criterion':['squared_error', 'friedman_mse'],
+                # 'max_features':['auto','sqrt','log2'],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            },
+            "LogisticRegression": {},
+            "AdaBoostClassifier": {
+                'learning_rate': [.1, .01, .001],
+                'n_estimators': [8, 16, 32, 64, 128, 256]
+            }
+
+        }
+
+        model_report: dict = evaluate_models(X_train=x_train, y_train=y_train, X_test=x_test, y_test=y_test, models = models, param = params)
+
+
+        ## To get the best model name from dict and best model score from dict
+        best_model_name = max(model_report, key=model_report.get)
+        best_model_score = max(sorted(model_report.values()))
+
+        best_model = models[best_model_name]
+
+        y_train_pred = best_model.predict(x_train)
+        classification_train_metric = get_classification_score(y_train, y_train_pred)
+
+        ## Tracking mlflow
+        self.track_mlflow(best_model, classification_train_metric) #To track it by susing UI, write mlflow ui on terminal
+
+        y_test_pred = best_model.predict(x_test)
+        classification_test_metric = get_classification_score(y_test, y_test_pred)
+
+        preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
+        model_dir_path = os.path.dirname(self.model_trainer_config.trained_model_file_path)
+        os.makedirs(model_dir_path, exist_ok=True)
+
+        Network_Model = NetworkModel(preprocessor=preprocessor,model= best_model)
+        save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
+
+        ## Model Trainer Artifact
+        model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path= self.model_trainer_config.trained_model_file_path,
+                             train_metric_artifact=classification_train_metric,
+                             test_metric_artifact=classification_test_metric)
+
+        logging.info(f"Model Trainer artifact: {model_trainer_artifact}")
+        return model_trainer_artifact
+
+
+
+    def initiate_model_trainer(self):
+        try:
+            train_file_path = self.data_transformation_artifact.transformed_train_file_path
+            test_file_path = self.data_transformation_artifact.transformed_test_file_path
+
+            #Loading training and testing array
+            train_arr = load_numpy_array_data(train_file_path)
+            test_arr = load_numpy_array_data(test_file_path)
+
+            x_train, y_train, x_test, y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1],
+            )
+
+            model_trainer_artifact = self.train_model(x_train, y_train, x_test, y_test)
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
