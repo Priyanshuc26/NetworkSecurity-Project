4ed097e7c44f56fa19840b51f1d3899265226734
Priyanshuc26 <priyanshuc2606@gmail.com>
2026-01-29 21:44:19 +0530

    mlflow, training pipeline and app.py created

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -23,7 +23,11 @@ AdaBoostClassifier,
 GradientBoostingClassifier,
 )
 from sklearn.metrics import r2_score
+
 import mlflow
+import dagshub
+dagshub.init(repo_owner='Priyanshuc26', repo_name='NetworkSecurity-Project', mlflow=True)
+
 
 
 class ModelTrainer:
@@ -111,6 +115,9 @@ class ModelTrainer:
 
         Network_Model = NetworkModel(preprocessor=preprocessor,model= best_model)
         save_object(self.model_trainer_config.trained_model_file_path, obj=Network_Model)
+
+        # model pusher
+        save_object("final_model/model.pkl", best_model)
 
         ## Model Trainer Artifact
         model_trainer_artifact = ModelTrainerArtifact(trained_model_file_path= self.model_trainer_config.trained_model_file_path,
